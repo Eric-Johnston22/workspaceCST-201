485a1bd672e73d31f26cad65988ba7fa62278da2
Eric-Johnston <eric.mjohnston@protonmail.com>
2023-02-07 00:59:51 -0500

    added input.txt file. finished basic outline

--- project6-BinarySearchTree/src/app/BinaryTree.java
+++ project6-BinarySearchTree/src/app/BinaryTree.java
@@ -2,9 +2,159 @@ package app;
 
 public class BinaryTree
 {
+	Node root;
+	int counter;
 
 	public static class Node
 	{
+		String data;
+		Node left, right;
 		
+		public Node(String data)
+		{
+			this.data = data;
+		}
 	}
+	
+	public void insert(Node node)
+	{
+		root = insertHelper(root, node);
+	}
+	private Node insertHelper(Node root, Node node)
+	{
+		String data = node.data;
+		
+		if(root == null)
+		{
+			root = node;
+			return root;
+		}
+		else if ((data.compareTo(root.data) < 0))
+		{
+			root.left = insertHelper(root.left, node);
+		}
+		else
+		{
+			root.right = insertHelper(root.right, node);
+		}
+		
+		return root;
+	}
+	
+	public void display()
+	{
+		counter = 1;
+		displayHelper(root);
+	}
+	private void displayHelper(Node root)
+	{
+		if(root != null)
+		{
+			displayHelper(root.left);
+			System.out.println(counter + ". " + root.data);
+			counter++;
+			displayHelper(root.right);
+		}
+	}
+	
+	public boolean search(String data)
+	{
+		counter = 0;
+		return searchHelper(root, data);
+	}
+	private boolean searchHelper(Node root, String data)
+	{
+		if(root == null)
+		{
+			return false;
+		}
+		else if(root.data.equals(data))
+		{
+			counter++;
+			return true;
+		}
+		else if (root.data.compareTo(data) > 0)
+		{
+			counter++;
+			return searchHelper(root.left, data);
+		}
+		else
+		{
+			counter++;
+			return searchHelper(root.right, data);
+		}
+	}
+	
+	public void remove(String data)
+	{
+		if(search(data))
+		{
+			removeHelper(root, data);
+		}
+		else
+		{
+			System.out.println(data + " could not be found.");
+		}
+	}
+	public Node removeHelper(Node root, String data)
+	{
+		if (root == null)
+		{
+			return root;
+		}
+		else if (data.compareTo(root.data) < 0)
+		{
+			root.left = removeHelper(root.left, data);
+		}
+		else if (data.compareTo(root.data) > 0)
+		{
+			root.right = removeHelper(root.right, data);
+		}
+		else
+		{
+			if (root.left == null && root.right == null)
+			{
+				root = null;
+			}
+			else if (root.right != null)
+			{
+				root.data = successor(root);
+				root.right = removeHelper(root.right, root.data);
+			}
+			else
+			{
+				root.data = predecessor(root);
+				root.left = removeHelper(root.left, root.data);
+			}
+		}
+		
+		return root;
+	}
+	
+	// find the least value below right child of this root node
+	private String successor(Node root)
+	{
+		root = root.right;
+		
+		while(root.left != null)
+		{
+			root = root.left;
+		}
+		return root.data;
+	}
+	private String predecessor(Node root)
+	{
+		root = root.left;
+		
+		while(root.right != null)
+		{
+			root = root.right;
+		}
+		return root.data;
+	}
+	
+	
 }
+
+
+
--- project6-BinarySearchTree/src/app/Main.java
+++ project6-BinarySearchTree/src/app/Main.java
@@ -1,11 +1,78 @@
 package app;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
-		// TODO Auto-generated method stub
+		BinaryTree myTree = new BinaryTree();
+		ArrayList<String> myList = new ArrayList<String>();
+		Scanner fileIn = new Scanner(new File("input.txt"));
+		Scanner userIn = new Scanner(System.in);
+		String input = "";
+		
+		// Add contents of file to myList
+		while (fileIn.hasNext())
+		{
+			myList.add(fileIn.next());
+		}
+		// Close input
+		fileIn.close();
+		
+		// Insert data into tree
+		for (String i : myList)
+		{
+			myTree.insert(new BinaryTree.Node(i));
+		}
+		
+		
+		myTree.display();
+		
+		// User input for string search
+		while (true) 
+		{
+			System.out.println("\nEnter a string, or enter 0 to quit: ");
+			input = userIn.nextLine();
+			
+			if (input.equals("0"))
+			{
+				break;
+			}
+			
+			System.out.println(myTree.search(input) ? "'" + input +"'" + " located": "'" + input +"'" + " not in tree");
+			System.out.println("Inspected " + myTree.counter + " elements");
+			
+		}
+		
+		// User input for string removal
+		while (true)
+		{
+			System.out.println("\nEneter a string to remove, or enter 0 to quit: ");
+			
+			input = userIn.nextLine();
+			
+			if (input.equals("0"))
+			{
+				break;
+			}
+			
+			if (myTree.search(input))
+			{
+				System.out.println("Removing " + input);
+				myTree.remove(input);
+			}
+			else
+			{
+				System.out.println(input + " Not found");
+			}
+			
+			myTree.display();
+		}
 
 	}
 
